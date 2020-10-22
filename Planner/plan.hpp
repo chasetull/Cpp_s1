d42d9b1f8374f67528ad155220f29638da0d2bf0
chasetull <chasetullar14@gmail.com>
2020-10-21 19:47:10 -0600

    ass7 sup

--- Planner/plan.hpp
+++ Planner/plan.hpp
@@ -21,11 +21,13 @@ class Planner{
         ~Planner(); //destr
         void addAss(string, string);
         void deleteAss(string, string);
+        void deleteClass(string);
         void saveall(string);
         void addClass(string);
         void printall();
         ClassNode* searchClass(string);
         bool isSave();
+        void readData(string);
 };
 
 void printAss(ClassNode*);
